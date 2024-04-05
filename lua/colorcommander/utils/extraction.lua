842f1660b7a69ff0b1623a7e4c4b7f61588856af
Hecdin <Hecdinnicola@outlook.com>
2024-04-05 11:25:38 -0400

    v2.0.0-wip: Make small changes

--- lua/colorcommander/utils/extraction.lua
+++ lua/colorcommander/utils/extraction.lua
@@ -1,7 +1,9 @@
+local vLog = require("colorcommander.log").info
+
 local M = {}
 local O = require('colorcommander.utils.config')
 local U = require('colorcommander.utils.scripts')
-local CNT = require('colorcommander.utils.controller')
+local C = require('colorcommander.utils.controller')
 local rgb = require("colorcommander.converters.color_rgb")
 local hsl = require("colorcommander.converters.color_hsl")
 local lch = require("colorcommander.converters.color_lch")
@@ -10,17 +12,17 @@ local lch = require("colorcommander.converters.color_lch")
 M.get_hex_value = function(line_content, virtual_text)
   local res = nil
 
+  -- Convert the RGB color to HEX
   if string.match(line_content, "rgb%(%d+, %d+, %d+%)") then
-    -- Convert the RGB color to HEX
     res = U.convert_color_to_hex(line_content, "rgb%((%d+), (%d+), (%d+)%)", rgb.rgbToHex, virtual_text)
+  -- Convert the RGB color to HSL
   elseif string.match(line_content, "hsl%(%d+, %d+%p?, %d+%p?%)") then
-    -- Convert the RGB color to HSL
     res = U.convert_color_to_hex(line_content, "hsl%((%d+), (%d+)%p?, (%d+)%p?%)", hsl.hslToHex, virtual_text)
-  elseif string.match(line_content, "lch%(%d*%.?%d+%p? %d*%.?%d+ %d*%.?%d+%)") then
-    -- Convert the RGB color to LCH
-    res = U.convert_color_to_hex(line_content, "lch%((%d*%.?%d+)%p? (%d*%.?%d+) (%d*%.?%d+)%)", lch.lchToHex, virtual_text)
+  -- Convert the RGB color to LCH
+  elseif string.match(line_content, "lch%(%d+%.?%d+%p? %d+%.?%d+ %d+%.?%d+%)") then
+    res = U.convert_color_to_hex(line_content, "lch%((%d+%.?%d+)%p? (%d+%.?%d+) (%d+%.?%d+)%)", lch.lchToHex, virtual_text)
+  -- Extract the hexadecimal color
   elseif string.match(line_content, "#[%x][%x][%x][%x][%x][%x]") then
-    -- Extract the hexadecimal color
     res = line_content:match("#[%x][%x][%x][%x][%x][%x]")
     -- If virtual text is provided, convert the hexadecimal color to another format
     if virtual_text ~= nil then
@@ -28,8 +30,6 @@ M.get_hex_value = function(line_content, virtual_text)
       table.insert(virtual_text, value)
       res = value
     end
-  else
-    res = nil
   end
 
   return res
@@ -38,15 +38,15 @@ end
 M.hex_to = function(options, target)
   -- If the target format is LCH
   if options == 'lch' then
-    local l, c, h = CNT.hexToLch(target)
+    local l, c, h = C.hexToLch(target)
     return 'lch('..l ..'% ' ..c ..' ' ..h ..')'
   -- If the target format is HSL
   elseif options == 'hsl' then
-    local h, s, l = CNT.hexToHsl(target)
+    local h, s, l = C.hexToHsl(target)
     return 'hsl(' ..h ..', ' ..s ..'%, ' ..l ..'%)'
   -- If the target format is RGB
   elseif options == 'rgb' then
-    local r, g, b = CNT.hexToRgb(target)
+    local r, g, b = C.hexToRgb(target)
     return 'rgb(' ..r ..', ' ..g ..', ' ..b ..')'
   -- If the target format is HEX
   else

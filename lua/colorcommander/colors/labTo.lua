73cac2eeff0cd9a7a798f67264c1661db0abd597
Hecdin <Hecdinnicola@outlook.com>
2024-04-11 12:07:10 -0400

    2.0.2 (fix): Fix convert colors to LCH

--- lua/colorcommander/colors/labTo.lua
+++ lua/colorcommander/colors/labTo.lua
@@ -1,13 +1,12 @@
 local M = {}
-local utils = require("colorcommander.misc.utils")
 
 M.to_lch = function(l, a, b)
-    local c = math.sqrt(a*a + b*b)
-    local h = math.atan2(b, a)
-    if h < 0 then h = h + 2*math.pi end
-    h = h * 180 / math.pi
+  local c = math.sqrt(a*a + b*b)
+  local h = math.atan2(b, a)
+  if h < 0 then h = h + 2*math.pi end
+  h = h * 180 / math.pi
 
-    return l, c, h
+  return l, c, h
 end
 
 return M
--- lua/colorcommander/misc/converter.lua
+++ lua/colorcommander/misc/converter.lua
@@ -37,60 +37,60 @@ M.color_value = function(line_content, virtual_text, mode)
         else
           result[#result + 1] = input
         end
-      -- to RGB
+        -- to RGB
       elseif target_format == "rgb" then
+        local text_style = 'rgb(%d, %d, %d)'
         if current_format == "#" then
           n1, n2, n3 = hex.to_rgb(input)
-          result[#result + 1] = string.format('rgb(%d, %d, %d)', n1, n2, n3)
+          result[#result + 1] = string.format(text_style, n1, n2, n3)
         elseif current_format == "hsl" then
           for x, y, z in string.gmatch(input, format.value) do
             n1, n2, n3 = hex.to_rgb(hsl.to_hex(tonumber(x), tonumber(y), tonumber(z)))
-          result[#result + 1] = string.format('rgb(%d, %d, %d)', n1, n2, n3)
+            result[#result + 1] = string.format(text_style, n1, n2, n3)
           end
         elseif current_format == "lch" then
           for x, y, z in string.gmatch(input, format.value) do
             n1, n2, n3 = hex.to_rgb(lch.to_hex(tonumber(x), tonumber(y), tonumber(z)))
-          result[#result + 1] = string.format('rgb(%d, %d, %d)', n1, n2, n3)
+            result[#result + 1] = string.format(text_style, n1, n2, n3)
           end
         else
           result[#result + 1] = input
         end
-      -- to HSL
+        -- to HSL
       elseif target_format == "hsl" then
+        local text_style = 'hsl(%d, %d, %d)'
         if current_format == "rgb" then
           for x, y, z in string.gmatch(input, format.value) do
             n1, n2, n3 = rgb.to_hsl(tonumber(x), tonumber(y), tonumber(z))
-            result[#result + 1] = string.format('hsl(%d, %d, %d)', n1, n2, n3)
+            result[#result + 1] = string.format(text_style, n1, n2, n3)
           end
         elseif current_format == "lch" then
           for x, y, z in string.gmatch(input, format.value) do
             n1, n2, n3 = rgb.to_hsl(hex.to_rgb(lch.to_hex(tonumber(x), tonumber(y), tonumber(z))))
-            result[#result + 1] = string.format('hsl(%d, %d, %d)', n1, n2, n3)
+            result[#result + 1] = string.format(text_style, n1, n2, n3)
           end
         elseif current_format == "#" then
           n1, n2, n3 = rgb.to_hsl(hex.to_rgb(input))
-          result[#result + 1] = string.format('hsl(%d, %d, %d)', n1, n2, n3)
+          result[#result + 1] = string.format(text_style, n1, n2, n3)
         else
           result[#result + 1] = input
         end
-      -- to LCH
+        -- to LCH
       elseif target_format == "lch" then
+        local text_style = 'lch(%.1f%% %.1f %.1f)'
         if current_format == "#" then
           n1, n2, n3 = hex.to_rgb(input)
-          n1, n2, n3 = rgb.to_xyz(n1, n2, n3)
-          n1, n2, n3 = xyz.to_lab(n1, n2, n3)
-          n1, n2, n3 = lab.to_lch(n1, n2, n3)
-          -- n1, n2, n3 = lab.to_lch(xyz.to_lab(rgb.to_xyz(n1, n2, n3)))
-            result[#result + 1] = string.format('lch(%d%% %d %d)', n1, n2, n3)
+          n1, n2, n3 = lab.to_lch(xyz.to_lab(rgb.to_xyz(n1, n2, n3)))
+          result[#result + 1] = string.format(text_style, n1, n2, n3)
         elseif current_format == "rgb" then
           for x, y, z in string.gmatch(input, format.value) do
             n1, n2, n3 = lab.to_lch(xyz.to_lab(rgb.to_xyz(tonumber(x), tonumber(y), tonumber(z))))
-            result[#result + 1] = string.format('lch(%d%% %d %d)', n1, n2, n3)
+            result[#result + 1] = string.format(text_style, n1, n2, n3)
           end
         elseif current_format == "hsl" then
           for x, y, z in string.gmatch(input, format.value) do
             n1, n2, n3 = lab.to_lch(xyz.to_lab(rgb.to_xyz(hex.to_rgb(hsl.to_hex(tonumber(x), tonumber(y), tonumber(z))))))
-            result[#result + 1] = string.format('lch(%d%% %d %d)', n1, n2, n3)
+            result[#result + 1] = string.format(text_style, n1, n2, n3)
           end
         else
           result[#result + 1] = input
